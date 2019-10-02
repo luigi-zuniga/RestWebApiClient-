e8168c2a6c0b51fd036180ca2861746224b50a76
luigi.zuniga-innova-ti.cl <luis.zuniga@innova-ti.cl>
2019-10-02 13:01:22 -0300

    ASP.NET REST API First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RestWebApiClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            ProcessRepositories().Wait();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var msg = await stringTask;
+
+            Console.Write(msg);
+        }
+    }
+}
